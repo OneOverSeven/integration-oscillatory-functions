e9d8d5ce1ded2b6e8ba05a0d62f0dd8557a8c4eb
OneOverSeven <max.loge@gmail.com>
2017-04-21 12:52:36 +0200

    new scripts

---
+++ Matlab Scripts/ex2new.m
@@ -0,0 +1,118 @@
+function [] = ex2new()
+n=2000;
+x=zeros(n+1,1);
+x2=zeros(2*(n+1),1);
+vec_c=ones(n+1,1);
+vec_c2=ones(2*(n+1),1);
+vec_c(1,1)=2;
+vec_c(n+1,1)=2;
+vec_c2(1,1)=2;
+vec_c2(2*(n+1),1)=2;
+vec_a=zeros(2*(n+1),1);
+vec_temp1=zeros(2*(n+1),1);
+vec_temp2=zeros(n+1,1);
+omega=500;
+
+indice1=zeros(n+1,n+1);
+indice2=zeros(2*(n+1),2*(n+1));
+D=zeros(n+1,n+1);
+S=0.5*eye(n+1,n+1);
+M=zeros(n+1,n+1);
+M1=zeros(n+1,n+1);
+M2=zeros(n+1,n+1);
+vec_f=zeros(n+1,1);
+
+func_a=@(x) 1i*omega*(x+2)/2;
+func_f=@(x) sin((x+1)/2)/2;
+
+
+
+
+for s = 1 : n+1
+x(s) = cos(pi*((s-1)/n));
+end
+
+for s = 1 : 2*(n+1)
+    for j = 1 : 2*(n+1)
+        vec_temp1(j,1) = ((1/vec_c2(j,1))*func_a(x2(j,1))*cos((s-1)*(j-1)*pi/((2*n)+1)));
+    end
+    sumvectemp1 = sum(vec_temp1);
+    vec_a(s,1) = (2/(vec_c2(s,1)*2*(n+1)))*sumvectemp1;
+end
+
+for s = 1 : n+1
+    for j = 1 : n+1
+        vec_temp2(j,1) = ((1/vec_c(j,1))*func_f(x(j,1))*cos((s-1)*(j-1)*pi/n));
+    end
+    sumvectemp2 = sum(vec_temp2);
+    vec_f(s,1) = (2/(vec_c(s,1)*(n+1)))*sumvectemp2;
+end
+
+vec_a(1,1)=2*vec_a(1,1);
+vec_a(2*(n+1),1)=2*vec_a(n+1,1);
+
+for i = 1 : n+1
+    for j = 1 : n+1
+   indice1(i,j)=abs(j-i);
+   indice2(i,j)=i+j-2;
+    end
+    indice2(1,i)=0;
+end
+
+for i = 1 : 2*(n+1)
+    for j = 1 : 2*(n+1)
+   indice2(i,j)=i+j-2;
+    end
+    indice2(1,i)=0;
+end
+
+indice1=indice1+ones(n+1,n+1);
+
+for i = 1 : n+1
+    for j = 1 : n+1
+    M1(i,j)=vec_a(indice1(i,j));
+    end
+end
+
+for i = 2 : n+1
+    for j = 1 : n+1
+    M2(i,j)=vec_a(indice2(i,j));
+    end
+end
+
+for i = 1 : n-1
+    for j = 1 : n+1
+    S(i,i+2)=-0.5;
+    end
+end
+
+for i = 1 : n
+    D(i,i+1)=i;
+end
+M=M1+M2;
+B=(D+(S*M));
+u=zeros(n+1,1);
+u=(B\(S*vec_f));
+ub=sum(u)
+vua=zeros(n+1,1);
+
+for k = 1: n+1
+    vua(k,1)=u(k,1)*(-1).^(k-1);
+end
+ua=sum(vua)
+result1=(ub-ua*exp(1i*1000))
+
+% [U,S,V] = svd(B);
+% P2=((V')\(S\(U\vec_f)));
+% [L,UU] = lu(B);
+% P3=(UU\L\vec_f);
+% result1=(P1(1)-P1(n+1)*exp(1i*1000));
+% result2=(P2(1)-P2(n+1)*exp(1i*1000));
+% result3=(P3(1)-P3(n+1)*exp(1i*1000));
+% result1
+% result2
+% result3
+% D
+% diag
+% B
+% f
